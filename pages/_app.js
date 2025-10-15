2cbfd5b550c3002525dc3b627c2e3d2b10909e02
sinexx <sine.thitikan@gmail.com>
2025-10-15 11:25:40 +0700

    fix: load LIFF SDK with next/script (afterInteractive)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -7,6 +8,13 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
       </Head>
+
+      {/* Load LIFF SDK asynchronously on the client to avoid synchronous script rules */}
+      <Script
+        src="https://static.line-scdn.net/liff/edge/2/sdk.js"
+        strategy="afterInteractive"
+      />
+
       <Component {...pageProps} />
     </>
   );
